76f5860d3c8abf6428d65259f8a2aff444994c85
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-04 09:13:52 +0200

    continue button for new game

--- react-project-2/src/component/game-board.jsx
+++ react-project-2/src/component/game-board.jsx
@@ -11,11 +11,10 @@ function GameBoard(props) {
         
         if (num === 100 && !finished) {
             setFinished(true)
+            alert(`${props.user.name} finished!`);
             // props.usersArr.filter((item, index) => {
             //     item.name !== props.user.name
             // })
-            console.log(`${num} is 100?`);
-            console.log(props.usersArr);
             
             props.setUsersArr(props.usersArr);
         }
@@ -30,7 +29,19 @@ function GameBoard(props) {
       <h3>name: {props.user.name}</h3>
       <h3>number: {number}</h3>
       <h3>steps: {steps}</h3>
-      {finished ? <button onClick={props.changeTurn(props.id)}>hi</button> : (
+      {finished ? (
+        <>
+        
+    <button onClick={() => {
+      props.changeTurn(props.id)
+    }}>finish</button> <button onClick={() => {
+        setNumber(Math.floor(Math.random() * 100))
+        console.log(number);
+        setFinished(false);
+        setSteps(0)
+    }}>continue</button> 
+        </>
+        ) : (
         <>
       <button disabled={props.turn === props.id ? false : true} onClick={() => {
           setNumber(number + 1)
